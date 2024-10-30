c8180a640cd880f7b201bbc8cf6e4487a5340c53
MONK <169875752+DevNChill@users.noreply.github.com>
2024-10-30 09:16:47 +0530

    changed illuminate plugin by adding a defer function

--- nvim/lua/plugins/vim-illuminate.lua
+++ nvim/lua/plugins/vim-illuminate.lua
@@ -3,9 +3,14 @@ return {
 	lazy = false,
 	config = function()
 		require("illuminate").configure({})
-		-- Set custom highlight groups
-		vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#3e4451", underline = true })
-		vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#3e4451", underline = true })
-		vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#3e4451", underline = true })
+
+		-- Apply highlight settings after colorscheme
+		vim.defer_fn(function()
+			vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#3e4451", underline = true })
+			vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#3e4451", underline = true })
+			vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#3e4451", underline = true })
+			-- Add enlarging effect by using the same highlighting as 'CursorLine' or a custom highlight group
+			vim.api.nvim_set_hl(0, "CursorLine", { bg = "#ffffff", bold = true })
+		end, 0)
 	end,
 }
