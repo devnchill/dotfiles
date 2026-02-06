e24889f71d692010dd456cca3deb83453f007365
Viena <169875752+devnchill@users.noreply.github.com>
2026-02-06 11:56:42 +0530

    add tmux config

--- users/viena/home.nix
+++ users/viena/home.nix
@@ -16,6 +16,7 @@
   home.file.".config/niri".source = ./config/niri;
   home.file.".config/waybar".source = ./config/waybar;
   home.file.".config/nvim".source = ./config/nvim;
+  home.file.".tmux.conf".source = ./config/tmux/.tmux.conf;
   home.file.".local/bin".source = ./bin;
 
   programs.neovim = {
