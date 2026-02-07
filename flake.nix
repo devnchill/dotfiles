b5ce214d3649af4d4280f30e66ed420bc75f50ae
Viena <169875752+devnchill@users.noreply.github.com>
2026-02-07 10:12:04 +0530

    restructure system configuration

--- flake.nix
+++ flake.nix
@@ -1,27 +1,35 @@
 {
-    description = "My NisOS system flake";
+  description = "My NixOS system flake";
 
-    inputs = {
-        nixpkgs.url = "github:nixOS/nixpkgs?ref=nixos-unstable";
-        home-manager.url="github:nix-community/home-manager";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-		spicetify-nix.url = "github:Gerg-L/spicetify-nix";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixOS/nixpkgs?ref=nixos-unstable";
 
-    outputs = inputs@{self,nixpkgs,home-manager,...}:{
-        nixosConfigurations.nixosbtw = nixpkgs.lib.nixosSystem{
-            system = "x86_64-linux";
-            modules = [
-                ./configuration.nix 
-                home-manager.nixosModules.home-manager
-		{
-		    home-manager.useGlobalPkgs = true;
-		    home-manager.useUserPackages = true;
-		    home-manager.backupFileExtension = "backup";
-		    home-manager.users.viena = ./users/viena/home.nix;
-		    home-manager.extraSpecialArgs = { inherit inputs; };
-		}
-            ];
-        };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+  let
+    hmConfig = {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.backupFileExtension = "backup";
+      home-manager.users.viena = ./users/viena/home.nix;
+      home-manager.extraSpecialArgs = { inherit inputs; };
     };
+  in
+  {
+    nixosConfigurations.nixosbtw =
+      nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/nixosbtw/configuration.nix
+          home-manager.nixosModules.home-manager
+          hmConfig
+        ];
+      };
+  };
 }
+
