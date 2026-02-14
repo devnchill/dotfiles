57e76718b95ee63469350c5361865bc328dd3109
Viena <169875752+devnchill@users.noreply.github.com>
2026-02-14 21:37:42 +0530

    nvim: slap treesitter autocmd

--- hosts/nixosbtw/users/viena/config/nvim/lua/config/autocmds.lua
+++ hosts/nixosbtw/users/viena/config/nvim/lua/config/autocmds.lua
@@ -4,3 +4,10 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     require("conform").format({ bufnr = args.buf })
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    pcall(vim.treesitter.start, args.buf)
+  end
+})
+
